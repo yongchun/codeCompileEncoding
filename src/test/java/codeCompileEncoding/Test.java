9f7f6b75aef971a5923b4a1d12fd2c7c78ad37c1
yongchun <yongchun.chengyc@gmail.com>
2013-12-03 15:51:04 +0800

    add test

---
+++ src/test/java/codeCompileEncoding/Test.java
@@ -0,0 +1,11 @@
+package codeCompileEncoding;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: chengyongchun
+ * Date: 13-12-3
+ * Time: 下午3:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
